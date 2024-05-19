f6fcea6b5042a0a1e77f7c8eb5bd77c587cf3c23
Lisa Dore <e1617999@cmaisonneuve.qc.ca>
2024-05-19 14:14:12 -0400

    coding de l'exercice 2 (mostly done)

--- DoreLisa_TP2_Exercice2.py
+++ DoreLisa_TP2_Exercice2.py
@@ -1,4 +1,25 @@
 import csv
 
-with open('pokemon.csv', 'r') as pokedex_file:
-    pokedex = csv.reader(pokedex_file)
+
+def lecture_poke_stats(csv_file):
+    pokedex_dict = {}
+    with open('pokemon.csv', 'r') as pokedex_file:
+        pokedex_reader = csv.reader(pokedex_file, delimiter=',')
+
+        for ligne in pokedex_reader:
+            poke_nom = ligne[0]
+            poke_stats = list(map(int, ligne[1:]))
+            pokedex_dict[poke_nom] = poke_stats
+
+    return pokedex_dict
+
+
+pokedex = lecture_poke_stats('pokemon.csv')
+for nom, stats in pokedex.items():
+    print(f"{nom} : {stats}")
+
+pkmn = lecture_poke_stats("pokemon.csv")
+print(isinstance(pkmn, dict))
+print(isinstance(pkmn["Pikachu"], list))
+print(isinstance(pkmn["Pikachu"][0], int))
+
